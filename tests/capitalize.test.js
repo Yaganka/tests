143c998395b84054a71c6fd76a631e491adac0c1
Anna Yagofarova <nuta.max4@gmail.com>
2022-12-13 22:47:34 +0300

    third commit

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,4 +1,4 @@
-import { capitalize } from './..src/capitalize.js';
+import { capitalize } from '../src/capitalize.js';
 
 if (capitalize('hello') !== 'Hello') {
   throw new Error('Функция работает неверно!');
@@ -7,4 +7,5 @@ if (capitalize('hello') !== 'Hello') {
 if (capitalize('') !== '') {
   throw new Error('Функция работает неверно!');
 }
+
 console.log('Все тесты пройдены!');
